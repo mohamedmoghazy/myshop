a5fd71d1b27efb20811a52ac8c490c7c36c2335e
Mohamed Moghazi <eng.moghazy@gmail.com>
2024-08-03 23:53:53 +0200

    install react with vite instead

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeDisplay from './displays/HomeDisplay'
 
-const App = () =>
-{
+function App() {
   return (
     <>
       <Header></Header>
