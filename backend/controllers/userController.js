3967411263f81e7d1754451ebcb86f7a12a5aaae
Mohamed Moghazi <eng.moghazy@gmail.com>
2024-08-27 10:06:25 +0200

    added get user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 // @desc   Auth user & get token
 // @route  POST /api/users/login
@@ -12,19 +13,9 @@ const login = asyncHandler(async (req, res) =>
 
     if (user && (await user.matchPassword(password)))
     {
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '30d',
-        });
+        generateToken(res, user._id);
 
-        //set JWT as HTTP only cookie
-        res.cookie('sparktimetoken', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
-        });
-
-        res.json(
+        res.status(200).json(
             {
                 _id: user._id,
                 name: user.name,
@@ -46,7 +37,38 @@ const login = asyncHandler(async (req, res) =>
 // @access Public
 const registerUser = asyncHandler(async (req, res) =>
 {
-    res.send('register user');
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+
+    if (userExists)
+    {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user)
+    {
+        generateToken(res, user._id);
+
+        res.status(201).json(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+            });
+    }
+    else
+    {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 // @desc   logout user / clear cookie
@@ -67,7 +89,22 @@ const logoutUser = asyncHandler(async (req, res) =>
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) =>
 {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user)
+    {
+        res.status(200).json(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+            });
+    } else
+    {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc   Update user profile
